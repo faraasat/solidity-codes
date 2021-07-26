4d1234ba17fad986f23882aa1da401a9709b8695
Farasat Ali <farasataliazeemi1@gmail.com>
2021-07-26 09:36:59 +0500

    Update 28_low-level-calls.sol

--- 28_low-level-calls.sol
+++ 28_low-level-calls.sol
@@ -8,8 +8,8 @@ pragma solidity >= 0.8.0;
 contract Calculator {
     
     function add(address math, uint a, uint b) public returns (bool success, bytes memory result) {
-        bytes memory method = abi.encodeWithSignature("add(uint,uint)", a, b);  // Convert to binary and return hash (encode and signature with keccak)
-        (success, result) = math.call(method);  // to call a function of another contract whose address is given. In this we are giving low level byte code to execute.
+        bytes memory method = abi.encodeWithSignature("add(uint256,uint256)", a, b);  // Convert to binary and return hash (encode and signature with keccak)
+        (success, result) = math.call(method);  // to call a function of another contract whose address is given. In this we are giving low level byte code to execute. It matches the signature given in method variable with the signature of method in deplyed contract and run that instructions.
     }
     
 }
